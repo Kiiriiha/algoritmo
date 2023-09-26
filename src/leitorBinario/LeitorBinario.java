6b03247fe93ff40c65599afc3ffbcb02f3ef2f6c
Gabriel Pacheco <hisokasasaki@gmail.com>
2023-09-26 00:10:46 -0300

    numero em binario

---
+++ src/leitorBinario/LeitorBinario.java
@@ -0,0 +1,31 @@
+package leitorBinario;
+
+public class LeitorBinario {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int converteStringBinary(String palavra){
+		    
+		    Integer numPalavra = new Integer(palavra);
+		    
+		    int i = Integer.parseInt(palavra);
+
+		    Integer.toBinaryString(i);
+
+		    return i;
+		}
+		
+
+		        int n = 31;
+		 
+		        String binary = Integer.toBinaryString(n);
+		 
+		        System.out.println(binary);
+		    
+		
+
+	}
+
+}
